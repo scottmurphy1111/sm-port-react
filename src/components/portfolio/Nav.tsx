29093ba4485016fb14aa693a72091bb0fed58490
scottmurphy1111 <scottmurphy1111@gmail.com>
2021-01-01 19:06:28 -0500

    nav menu mobile

--- src/components/portfolio/Nav.tsx
+++ src/components/portfolio/Nav.tsx
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Logo } from '../../assets/svgs';
 
 export default function VertNav() {
   const [show, setShow] = useState(false);
+  const [showMobileMenu, setShowMobileMenu] = useState(false)
+  const navItemsList = useRef<any>(null)
+  const navButtonWrapper = useRef<any>(null)
 
   useEffect(() => {
     setTimeout(() => {
@@ -44,19 +47,47 @@ export default function VertNav() {
     window.scrollTo({ top: scrollPosition, behavior: 'smooth' });
   })
 
+  const handleNavButtonClick = () => {
+    console.log('should open');
+    setShowMobileMenu(true);
+  }
+
+  const documentClick = () => {
+
+    document.addEventListener('click', (event) => {
+      console.log('evennt target', event.target);
+      console.log('is not UL', !navItemsList.current.contains(event.target));
+      console.log('is not BUTTON', !navButtonWrapper.current.contains(event.target));
+      if (!navItemsList.current.contains(event.target) && !navButtonWrapper.current.contains(event.target)) {
+        console.log('should close', );
+        setShowMobileMenu(false);
+      } else {
+        console.log('not target',);
+      }
+    })
+  }
+
+  useEffect(() => {
+   documentClick();
+  }, [])
+
+
   return (
     <div className={`nav ${show ? 'show' : ''}`}>
       <div className='container-fluid'>
         <div className="wrap">
 
           <div className='row'>
-            <div className='col-xs-3'>
+            <div className='col-xs-4 col-md-3'>
               <span className='logo' data-nav='home' onClick={handleClick}>
                 <Logo />
               </span>
             </div>
-            <div className='col-xs-5 col-xs-offset-4'>
-              <ul>
+            <div className='col-xs-8 col-md-5 col-md-offset-4 nav-wrapper'>
+              <div ref={navButtonWrapper} className="nav-button-wrapper" onClick={() => handleNavButtonClick()}>
+                <div className="nav-button"></div>
+              </div>
+              <ul ref={navItemsList} className="nav-items" data-active={showMobileMenu}>
                 {
                   navItems.map((item: any, index: number) => {
                     if (index !== 0) {
