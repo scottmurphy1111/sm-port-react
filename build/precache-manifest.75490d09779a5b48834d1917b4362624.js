06a583c6d1a4358840c4d8f80891a347f8e2b769
scottmurphy1111 <scottmurphy1111@gmail.com>
2021-01-04 15:58:44 -0500

    pushing up old portfolio changes

---
+++ build/precache-manifest.75490d09779a5b48834d1917b4362624.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155879bd28e631363edd4fb0f9677710",
+    "url": "/sm-port-react/index.html"
+  },
+  {
+    "revision": "2cc445cd59f248d4ae15",
+    "url": "/sm-port-react/static/css/main.b2d2ef87.chunk.css"
+  },
+  {
+    "revision": "ff5c29162e46c3edbd70",
+    "url": "/sm-port-react/static/js/2.d560b354.chunk.js"
+  },
+  {
+    "revision": "22944df67a1e9cf588daa5255143530c",
+    "url": "/sm-port-react/static/js/2.d560b354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cc445cd59f248d4ae15",
+    "url": "/sm-port-react/static/js/main.bf62ddec.chunk.js"
+  },
+  {
+    "revision": "2d83528658ea9ed9867f",
+    "url": "/sm-port-react/static/js/runtime-main.4cf05acd.js"
+  },
+  {
+    "revision": "3bb60a8ae87f0819ee4a8641b2720a78",
+    "url": "/sm-port-react/static/media/SourceSansPro-ExtraLight.3bb60a8a.ttf"
+  },
+  {
+    "revision": "b2e90cc01cdd1e2e6f214d5cb2ae5c26",
+    "url": "/sm-port-react/static/media/SourceSansPro-Light.b2e90cc0.ttf"
+  },
+  {
+    "revision": "ba6cad25afe01d394e830f548a7f94df",
+    "url": "/sm-port-react/static/media/SourceSansPro-Regular.ba6cad25.ttf"
+  },
+  {
+    "revision": "603934185ba4500d610a71d6fea6de8d",
+    "url": "/sm-port-react/static/media/bg-pattern-light.60393418.png"
+  },
+  {
+    "revision": "3cf818fefc94072ecc87291972c1acc6",
+    "url": "/sm-port-react/static/media/digi-bg1.3cf818fe.png"
+  },
+  {
+    "revision": "6a1c5f2105ec16fbafa48e4b047b37dc",
+    "url": "/sm-port-react/static/media/me-trans.6a1c5f21.png"
+  },
+  {
+    "revision": "6e35eb2309d7eea2141ffe32240835e1",
+    "url": "/sm-port-react/static/media/minus.6e35eb23.svg"
+  },
+  {
+    "revision": "a759dea05235a0476512a1606375c911",
+    "url": "/sm-port-react/static/media/plus.a759dea0.svg"
+  }
+]);
