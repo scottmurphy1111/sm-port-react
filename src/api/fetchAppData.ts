7b8ff018ed80bd35bf5ccdc123feba95d18625ae
Scott Murphy <smurphy@housecanary.com>
2022-09-17 00:47:08 -0400

    fetch appdata from cf

--- src/api/fetchAppData.ts
+++ src/api/fetchAppData.ts
@@ -1,10 +1,23 @@
-import data from '../data/data.json'
+// import data from '../data/data.json'
 
-export const fetchAppData = () => {
+export const fetchAppData = async () => {
   try {
-    const response = Promise.resolve(data)
-    const payload = response.then(data => data)
-    return payload
+    // const response = Promise.resolve(data)
+    // const payload = response.then(data => data)
+    // return payload
+    const response = await fetch(
+      'https://d18gqb4iyj0dp2.cloudfront.net/data.json',
+      {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': ' application/json',
+        },
+      }
+    )
+    console.log(response)
+    const appData = await response.json()
+    console.log(appData)
+    return appData
   } catch (error) {
     throw new Error("Couldn't return App Data")
   }
