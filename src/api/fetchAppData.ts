c467f983092e8b2f968920fe8d7fc49c2bb92f54
Scott Murphy <smurphy@housecanary.com>
2022-09-17 22:12:33 -0400

    envs to handle data fetching

--- src/api/fetchAppData.ts
+++ src/api/fetchAppData.ts
@@ -1,27 +1,26 @@
 import data from '../data/data.json'
 
+const CF_DATA_URL = 'https://d18gqb4iyj0dp2.cloudfront.net/data.json'
+
 export const fetchAppData = async () => {
   try {
-    // const response = Promise.resolve(data)
-    // const payload = response.then(data => data)
-    // return payload
-    const response = await fetch(
-      'https://d18gqb4iyj0dp2.cloudfront.net/data.json',
-      {
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': ' application/json',
-        },
-      }
-    )
-    console.log(response)
+    if (process.env.NODE_ENV === 'development') {
+      const response = Promise.resolve(data)
+      const payload = await response
+      return payload
+    }
+
+    const response = await fetch(CF_DATA_URL, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': ' application/json',
+      },
+    })
+
     const appData = await response.json()
-    console.log(appData)
+
     return appData
   } catch (error) {
     console.warn("Couldn't return App Data, using backup data", error)
-    const response = Promise.resolve(data)
-    const payload = response.then(data => data)
-    return payload
   }
 }
